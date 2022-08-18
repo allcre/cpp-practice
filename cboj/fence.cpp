8c47f222d07fb56f121abaf161080653c4d4d2e6
Allison <allisoncretel@gmail.com>
2022-08-18 16:04:47 -0400

    works but sometimes not idk

---
+++ cboj/fence.cpp
@@ -0,0 +1,30 @@
+#include <iostream> // fails sometimes idk
+using namespace std;
+
+int main() {
+    int base;
+    cin >> base;
+    long double area = 0;
+    float h = 0;
+
+    float sides[base + 1];
+
+    for (int i = 0; i <= base; i++) {
+        cin >> sides[i];
+    }
+
+    float sums[base];
+
+    for (int j = 0; j < base; j++) {
+        sums[j] = sides[j] + sides[j+1];
+    }
+
+    for (int k = 0; k < base; k++) {
+        cin >> h;
+        area += sums[k] * (h/2);
+    }
+
+    cout << area;
+    
+    return 0;
+}
